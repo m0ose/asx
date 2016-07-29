8f66501a0e93d21cb1e5ca7dca21a87361645393
m0ose <cody@simtable.com>
2016-07-29 16:49:31 -0600

    entire model now in worker

--- test/apps/navierWorkerMain.js
+++ test/apps/navierWorkerMain.js
@@ -2,22 +2,20 @@ import util from 'lib/util.js'
 
 var canvas, ctx
 
-class thisIsAfuckedUpWayToStartAModelcausedByTranspiler {
-  constructor () {
-    const opts = {patchSize: 4, minX: -64, maxX: 64, minY: -64, maxY: 64}
-    worker.postMessage({type: 'setup', value: opts})
-    setInterval(function () {
-      worker.postMessage({type: 'step'})
-      worker.postMessage({type: 'getImgData'})
-    }, 30)
+function start () {
+  const opts = {patchSize: 4, minX: -64, maxX: 64, minY: -64, maxY: 64}
+  worker.postMessage({type: 'setup', value: opts})
+  setInterval(function () {
+    worker.postMessage({type: 'step'})
+    worker.postMessage({type: 'getImgData'})
+  }, 30)
 
-    var canvas = document.createElement('canvas')
-    canvas.width = 129
-    canvas.height = 129
-    canvas.style.width = '800px'
-    ctx = canvas.getContext('2d')
-    util.addToDom(canvas)
-  }
+  var canvas = document.createElement('canvas')
+  canvas.width = 129
+  canvas.height = 129
+  canvas.style.width = '800px'
+  ctx = canvas.getContext('2d')
+  util.addToDom(canvas)
 }
 
 
@@ -27,11 +25,11 @@ worker.onmessage = (ev) => {
   // console.log('main recieved msg', ev)
   var msg = ev.data
   if (typeof msg === 'object') {
-    if (msg.type === 'imgData') { 
+    if (msg.type === 'imgData') {
       ctx.putImageData(msg.value, 0, 0)
     } else if (msg.type) {
       if (msg.type === 'ready') {
-        new thisIsAfuckedUpWayToStartAModelcausedByTranspiler()
+        start()
       }
     }
   }
