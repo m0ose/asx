92dbcf52353546298d11ec1ca5b4e9f8110c8153
m0ose <cody@simtable.com>
2016-08-03 12:15:22 -0600

    a start

---
+++ test/apps/tileDS.js
@@ -0,0 +1,39 @@
+import TileDataSet from 'lib/TileDataSet.js'
+import util from 'lib/util.js'
+import ColorMap from 'lib/ColorMap.js'
+import Model from 'lib/Model.js'
+
+function main () {
+  testTileDS()
+}
+
+function testTileDS () {
+  var ds = new TileDataSet()
+  console.log(ds)
+/*
+  // put colors onto a model
+  model.patches.importDataSet(ds, 'elev', true)
+  const cmap = ColorMap.Jet
+  const [min, max] = [ds.min(), ds.max()]
+  console.log('min:', min, 'max:', max)
+  for (const p of model.patches) {
+    p.setColor(cmap.scaleColor(p.elev, min, max))
+  }
+  model.once()
+  console.log(ds)
+  */
+}
+
+
+
+const model = new Model('layers', {
+  patchSize: 1,
+  minX: -100,
+  maxX: 355,
+  minY: 0,
+  maxY: 255
+})
+
+util.toWindow({ model })
+
+main()
