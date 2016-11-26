7638e68118cd30588400a87b7e6d3afc4bc0929d
backspaces <owen@backspaces.net>
2016-11-26 12:14:28 -0700

    THREE.js updates - build3modules: New script to convert THREE examples/js   libs into es3 modules - package.json: use above to create etc/threelibs via system transform - test.html: remove System.config use, not needed w/ above - test/apps/fire3.js: THREE updates

--- test/apps/fire3.js
+++ test/apps/fire3.js
@@ -3,39 +3,54 @@ import util from 'lib/util.js'
 import Color from 'lib/Color.js'
 import ColorMap from 'lib/ColorMap.js'
 import Model from 'lib/Model.js'
-// import * as THREE from 'etc/three.min.js'
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r82/three.min.js'
 
-const modules = { Color, ColorMap, Model, util, THREE }
+import * as THREE from 'etc/three.min.js'
+import OrbitControls from 'etc/threelibs/OrbitControls.js'
+
+// const modules = { Color, ColorMap, Model, util, THREE }
+const modules = { Color, ColorMap, Model, util }
 util.toWindow(modules)
 console.log(Object.keys(modules).join(', '))
 
 function initThree (canvas) {
   const renderer = new THREE.WebGLRenderer()
-  const [width, height] = [window.innerWidth, window.innerHeight]
-  renderer.setSize(width, height)
+  // const [innerWidth, innerHeight] = window
+  renderer.setSize(window.innerWidth, window.innerHeight)
 
   document.body.appendChild(renderer.domElement)
   // document.body.appendChild(canvas)
 
   const scene = new THREE.Scene()
 
-  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000)
+  const camera = new THREE.PerspectiveCamera(
+    45, window.innerWidth / window.innerHeight, 1, 1000)
+  camera.position.y = 200
   camera.position.z = 400
+  camera.lookAt(scene.position)
+  const controls = new OrbitControls(camera, renderer.domElement)
 
   const texture = new THREE.Texture(canvas)
-  texture.generateMipmaps = false
-  texture.minFilter = THREE.LinearFilter
-  texture.magFilter = THREE.LinearFilter
+  // texture.generateMipmaps = false
+  texture.minFilter = THREE.NearestFilter
+  texture.magFilter = THREE.NearestFilter
+  // texture.premultiplyAlpha = false
 
-  const geometry = new THREE.PlaneGeometry(canvas.width, canvas.height, 1, 1)
+  const geometry = new THREE.PlaneGeometry(
+    canvas.width, canvas.height, 10, 10)
+  geometry.vertices.forEach((v, i, verts) => {
+    v.z = util.randomInt(20)
+  })
+  console.log(geometry.vertices)
   const material = new THREE.MeshBasicMaterial({
-    map: texture, side: THREE.DoubleSide
+    map: texture, shading: THREE.FlatShading
+    //, side: THREE.DoubleSide//, wireframe: true
   })
   const mesh = new THREE.Mesh(geometry, material)
+  mesh.rotation.x = -Math.PI / 2
   scene.add(mesh)
 
-  return {scene, camera, renderer, texture, geometry, material, mesh}
+  return {
+    scene, camera, renderer, texture, geometry, material, mesh, controls }
 }
 
 class FireModel extends Model {
@@ -43,14 +58,16 @@ class FireModel extends Model {
     super(div, worldOptions, contextOptions)
     this.div.hidden = true
     this.threeObj = initThree(this.patches.pixels.ctx.canvas)
+    console.log(this.threeObj)
+    this.stopped = false
   }
   draw () {
     const pixels = this.patches.pixels
     pixels.ctx.putImageData(pixels.imageData, 0, 0)
-    const {renderer, scene, camera, texture, mesh} = this.threeObj
+    const { renderer, scene, camera, texture, controls } = this.threeObj
     texture.needsUpdate = true
-    mesh.rotation.x += 0.01
-    // mesh.rotation.y += 0.01
+
+    controls.update()
     renderer.render(scene, camera)
   }
 
@@ -79,23 +96,26 @@ class FireModel extends Model {
   }
 
   step () {
-    if (this.fires.length + this.embers.length === 0) {
-      console.log('Stopping:', this.anim.toString())
-      const percentBurned = this.burnedTrees / this.initialTrees * 100
-      console.log('Percent burned', percentBurned.toFixed(2))
-      this.stop()
-    }
+    if (!this.stopped) {
+      if (this.fires.length + this.embers.length === 0) {
+        console.log('Stopping:', this.anim.toString())
+        const percentBurned = this.burnedTrees / this.initialTrees * 100
+        console.log('Percent burned', percentBurned.toFixed(2))
+        console.log('3D navigator still running')
+        this.stopped = true
+      }
 
-    for (const p of this.fires) {
-      for (const n of p.neighbors4)
-        if (this.isTree(n))
-          this.ignight(n)
-      this.embers.setBreed(p)
-    }
-    this.fadeEmbers()
+      for (const p of this.fires) {
+        for (const n of p.neighbors4)
+          if (this.isTree(n))
+            this.ignight(n)
+        this.embers.setBreed(p)
+      }
+      this.fadeEmbers()
 
-    if (this.anim.ticks % 100 === 0)
-      console.log(this.anim.toString())
+      if (this.anim.ticks % 100 === 0)
+        console.log(this.anim.toString())
+    }
   }
 
   isTree (p) { return p.color.equals(this.treeColor) }
